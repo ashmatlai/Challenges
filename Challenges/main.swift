73fd9154c49634eadad8e3996931fbad57e0e748
Anna Shmatlai <shmatlay.anna@gmail.com>
2020-08-10 18:49:25 +0100

    Initial Commit

---
+++ Challenges/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Challenges
+//
+//  Created by Anna Shmatlai on 10/08/2020.
+//  Copyright © 2020 Anna Shmatlai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
